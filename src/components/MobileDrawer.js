7fb573d021570ba56c755625bbc53c1991628870
jjoung1128 <jinwoong.joung@gmail.com>
2019-05-05 11:16:40 -0400

    mobile nav

--- src/components/MobileDrawer.js
+++ src/components/MobileDrawer.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
-import NestedList from './NestedList';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import GotoIcon from '@material-ui/icons/ChevronRight';
+import SearchIcon from '@material-ui/icons/Search';
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { Grid, Row, Col } from 'react-flexbox-grid';
+// import NestedList from './NestedList';
+
+const Menu = styled.li`
+  padding: 0.5rem;
+  border-bottom: 1px solid rgba(112,112,112,0.7);
+  &:focus {
+    color: #DC9D1E;
+  }
+`
+const Text = styled.span`
+  font-size: 0.7rem;
+  color: #3D3D3D;
+`
 
 function MobileDrawer(props) {
   const [state, setState] = React.useState({
@@ -13,29 +30,48 @@ function MobileDrawer(props) {
     setState({ ...state, right: open });
   };
 
-  var arr = [];
-  props.menus.forEach(item => {
-    var obj = {item: item.title, subItems: []};
-    item.menu.forEach(element => {
-        if (element.subItems.length > 0) {
-            obj.subItems.push(...element.subItems);
-        } else {
-            var subItem = {name: element.item, link: element.link};
-            obj.subItems.push(subItem);
-        }
-    });
-    arr.push(obj);
-  });
+  // var arr = [];
+  // props.menus.forEach(item => {
+  //   var obj = {item: item.title, subItems: []};
+  //   item.menu.forEach(element => {
+  //       if (element.subItems.length > 0) {
+  //           obj.subItems.push(...element.subItems);
+  //       } else {
+  //           var subItem = {name: element.item, link: element.link};
+  //           obj.subItems.push(subItem);
+  //       }
+  //   });
+  //   arr.push(obj);
+  // });
 
   return (
     <div>
       <IconButton aria-label="Menu" onClick={toggleDrawer(true)}>
-        <MenuIcon fontSize="large"/>
+        <MenuIcon fontSize="large" style={{color: "#FFFFFF"}}/>
       </IconButton>
       <Drawer anchor="right" open={state.right} onClose={toggleDrawer(false)}>
-        {arr.map((menu, i) => (
+        <Grid fluid style={{minWidth: "18rem", padding: "2rem"}}>
+          <Row xs="center">
+            <Col xs={12}>
+              <div style={{borderBottom: "2px solid #DFAC44"}}>
+                <SearchIcon style={{color: "#DFAC44"}}></SearchIcon>
+              </div>
+              <ul>
+                {props.menus.map((menu, i) => (
+                  <Menu key={i}>
+                    <Link to={menu.path}>
+                      <Text>{menu.name}</Text>
+                      <GotoIcon style={{color: "rgba(112, 112, 112, 0.7)", float: "right"}}/>
+                    </Link>
+                  </Menu>
+                ))}
+              </ul>
+            </Col>
+          </Row>
+        </Grid>
+        {/* {arr.map((menu, i) => (
           <NestedList key={i} item={menu.item} link={menu.link} subItems={menu.subItems}></NestedList>
-        ))}
+        ))} */}
       </Drawer>
     </div>
   );
--- src/components/QuickMenu.js
+++ src/components/QuickMenu.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-// import Paper from '@material-ui/core/Paper';
-// import Grid from '@material-ui/core/Grid'
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
+import { Link } from 'gatsby'
 
 const MyCol = styled(Col)`
   padding: 0 0 1rem 1rem;
@@ -14,14 +11,20 @@ const MyPaper = styled.div`
   padding: 39% 0 !important;
   width: 100%;
   height: 100%;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.5;
+  }
 `
 
 function QuickMenu(props) {
   const gridItems = props.items.map((menu, i) =>
     <MyCol key={i} xs={6}>
-      <MyPaper style={{backgroundColor: menu.backgroundColor}}>
-        <h4 style={{fontSize: "2rem", fontWeight: "bold", color: menu.textColor, textAlign: "center"}}>{menu.text}</h4>
-      </MyPaper>
+      <Link to={menu.path}>
+        <MyPaper style={{backgroundColor: menu.backgroundColor}}>
+          <h4 style={{fontSize: "2rem", fontWeight: "bold", color: menu.textColor, textAlign: "center"}}>{menu.text}</h4>
+        </MyPaper>
+      </Link>
     </MyCol>
   );
 
