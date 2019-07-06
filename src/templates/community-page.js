dce0a4dc34f52a316f43bc4f39fe73e4b164c84e
Dowan Kim <dowan1041@gmail.com>
2019-07-06 17:25:48 -0400

    Update community-page.js
    Test

--- src/templates/community-page.js
+++ src/templates/community-page.js
@@ -28,7 +28,7 @@ export const CommunityPageTemplate = ({ title }) => {
                 <MainTitle color="#FFFFFF" style={{marginTop: "3rem"}}>{title}</MainTitle>
                 <Divider background="#DFAC44" style={{marginTop: "5rem"}}></Divider>
                 <MainDescription color="#FFFFFF">
-                  저희 교회는 1973년에 장영춘 원로목사께서 개척하여 '사도행전의 역사를 재현하는 교회'를 모토로 40년을 은혜 가운데 달려 왔습니다.
+                  Test 저희 교회는 1973년에 장영춘 원로목사께서 개척하여 '사도행전의 역사를 재현하는 교회'를 모토로 40년을 은혜 가운데 달려 왔습니다.
                   그 목회철학을 계승하여 2013년에 영광스런 3기사역의 항해를 시작하게 되었습니다. 
                   주님이 원하시는 바로 그 교회의 핵심은 한 영혼을 소중히 여기시는 주님의 마음을 잊지 않는 것입니다.
                   시간이 흘러도 본질을 놓치지 않는 교회, 역사가 깊어져도 본질이 변하지 않는 교회, 주님이 이 땅에 다시 오시는 날까지 주의 심장이 살아 숨시는 교회가 되길 소망합니다.
